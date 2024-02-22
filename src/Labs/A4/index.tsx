f3824ac57e6efa7605206221e5898204ac3b85b0
jlarosa02 <j2002dl@gmail.com>
2024-02-21 21:20:34 -0500

    first commit post lecture

--- src/Labs/A4/index.tsx
+++ src/Labs/A4/index.tsx
@@ -1,10 +1,20 @@
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
 
-function Assignment3() {
+function Assignment4() {
+    function sayHello() {
+        alert("Hello");
+      }
+    
     return (
         <div className="container">
             <h1>Assignment 4</h1>
+            <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
         </div>
     );
 }
 
-export default Assignment3;
+export default Assignment4;
