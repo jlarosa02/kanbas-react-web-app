2ec9e7cb1b21052566807dd5df8c597823feb335
jlarosa02 <j2002dl@gmail.com>
2024-03-01 13:37:07 -0500

    retrieved Kanbas from a3 branch

---
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./index.css";
+import { modules } from "../../Database";
+import { FaEllipsisV, FaCheckCircle, FaPlusCircle } from "react-icons/fa";
+import { useParams } from "react-router";
+function ModuleList() {
+  const { courseId } = useParams();
+  const modulesList = modules.filter((module) => module.course === courseId);
+  const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+  return (
+    <>
+      <span className="container d-flex overflow-x-auto justify-content-end gap-2 padding-bottom">
+          <button>Collapse All</button>
+          <button>View Progress</button>
+          <select>
+            <option>Publish All</option>
+            <option>Publish All Modules and Items</option>
+            <option>Publish Modules Only</option>
+            <option>Unpublish All</option>
+          </select>
+          <button className="buttonRed">+ Module</button>
+        </span>
+      <ul className="list-group wd-modules">
+        {modulesList.map((module) => (
+          <li
+            className="list-group-item"
+            onClick={() => setSelectedModule(module)}>
+            <div>
+              <FaEllipsisV className="me-2" />
+              {module.name}
+              <span className="float-end">
+                <FaCheckCircle className="text-success" />
+                <FaPlusCircle className="ms-2" />
+                <FaEllipsisV className="ms-2" />
+              </span>
+            </div>
+            {selectedModule._id === module._id && (
+              <ul className="list-group">
+                {module.lessons?.map((lesson) => (
+                  <li className="list-group-item">
+                    <FaEllipsisV className="me-2" />
+                    {lesson.name}
+                    <span className="float-end">
+                      <FaCheckCircle className="text-success" />
+                      <FaEllipsisV className="ms-2" />
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default ModuleList;
+
---
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -0,0 +1,18 @@
+import { Link, useLocation } from "react-router-dom";
+import "./index.css";
+function CourseNavigation() {
+  const links = ["Home", "Modules", "Piazza", "Grades", "Assignments", "Quizzes", "People", 
+  "Discussions", "Announcements", "Pages", "Files"];
+  const { pathname } = useLocation();
+  return (
+    <ul className="wd-navigation mr-5">
+      {links.map((link, index) => (
+        <li key={index} className={pathname.includes(link) ? "wd-active" : ""}>
+          <Link to={link}>{link}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default CourseNavigation;
+
