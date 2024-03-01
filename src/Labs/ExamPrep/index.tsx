ed8c970bfc0708b520d72ef4a270c02e826673e1
jlarosa02 <j2002dl@gmail.com>
2024-03-01 13:18:38 -0500

    need to pull from a3

---
+++ src/Labs/ExamPrep/index.tsx
@@ -0,0 +1,9 @@
+function ExamPrep() {
+    return (
+        <div className="container">
+            <h1>Exam Practice</h1>
+        </div>
+    );
+}
+
+export default ExamPrep;
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,5 +1,6 @@
 import Assignment3 from "./A3";
 import Assignment4 from "./A4";
+import ExamPrep from "./ExamPrep";
 import Nav from "../Nav";
 import { Routes, Route, Link } from "react-router-dom";
 
@@ -9,10 +10,12 @@ function Labs() {
         <Nav />
         <h1>Labs</h1>
         <Link to="/Labs/a3">Assignment 3</Link> |
-        <Link to="/Labs/a4">Assignment 4</Link>
+        <Link to="/Labs/a4">Assignment 4</Link> | 
+        <Link to="/Labs/ExamPrep">Exam</Link>
         <Routes>
           <Route path="/a3/*" element={<Assignment3 />} />
           <Route path="/a4" element={<Assignment4 />} />
+          <Route path="/ExamPrep" element={<ExamPrep />} />
         </Routes>
       </div>
     );
